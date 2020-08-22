927372617deb76edf67a962730da8ec6eb33fd87
Kent Koski <>
2020-08-22 08:26:51 -0700

    Added array function

--- aEightKyu.py
+++ aEightKyu.py
@@ -5,4 +5,19 @@ def problem(a):
         return (a * 50) + 6
 
 
+def array(string):
+    strings = string.split(',')
+    del strings[0]
+    if len(strings) > 0:
+        del strings[len(strings)-1]
+    result = ''
+    for i in strings:
+        result = result + str(i) + ' '
+    if len(result) > 0:
+        return result.strip()
+    else:
+        return None
+
+
 print(problem(1))
+print(array(''))
